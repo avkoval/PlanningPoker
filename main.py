4c8a24b232beb742559aa2c334440a2ed565ea9d
Oleksii Koval <alex@koval.kharkov.ua>
2024-08-17 16:11:34 +0300

    Add .pycheckers, serve `pp-front/public` as `/`

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app.mount("/", StaticFiles(directory="pp-front/public", html=True), name="static")
+templates = Jinja2Templates(directory="package_docs")
 
 
 @app.get("/items/{item_id}")
