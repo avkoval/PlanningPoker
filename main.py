74d7faa4386386b0a1f7f02259d2d4bfa2d543a4
Oleksii Koval <alex@koval.kharkov.ua>
2024-08-18 22:57:46 +0300

    Google Auth / login.

--- main.py
+++ main.py
@@ -1,14 +1,73 @@
-from typing import Union
+import os
 
-from fastapi import FastAPI
+from authlib.integrations.starlette_client import (  # type: ignore[import]
+    OAuth, OAuthError)
+from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from starlette.config import Config
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.responses import RedirectResponse
+
+# OAuth settings
+GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID') or None
+GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET') or None
+if GOOGLE_CLIENT_ID is None or GOOGLE_CLIENT_SECRET is None:
+    raise BaseException('Missing env variables')
+
+# Set up oauth
+config_data = {'GOOGLE_CLIENT_ID': GOOGLE_CLIENT_ID, 'GOOGLE_CLIENT_SECRET': GOOGLE_CLIENT_SECRET}
+starlette_config = Config(environ=config_data)
+oauth = OAuth(starlette_config)
+oauth.register(
+    name='google',
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={'scope': 'openid email profile'},
+)
 
 app = FastAPI()
-app.mount("/", StaticFiles(directory="pp-front/public", html=True), name="static")
-templates = Jinja2Templates(directory="package_docs")
+
+SECRET_KEY = os.environ.get('SECRET_KEY') or None
+if SECRET_KEY is None:
+    raise Exception('Missing SECRET_KEY')
+app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
+
+app.mount("/app/", StaticFiles(directory="pp-front/public", html=True), name="app")
+app.mount("/static/", StaticFiles(directory="static", html=True), name="static")
+favicon_path = 'static/favicon.ico'
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    if 'access_token' in request.session:
+        return RedirectResponse('/app/')
+    return templates.TemplateResponse(
+        name="index.html", context={'request': request}
+    )
+
+
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return FileResponse(favicon_path)
+
+
+@app.route('/login')
+async def login(request: Request):
+    redirect_uri = request.url_for('auth')  # This creates the url for the /auth endpoint
+    return await oauth.google.authorize_redirect(request, redirect_uri)
+
+
+@app.route('/auth')
+async def auth(request: Request):
+    try:
+        access_token = await oauth.google.authorize_access_token(request)
+    except OAuthError as e:
+        return templates.TemplateResponse(
+            name="index.html", context={'request': request, 'error': e}
+        )
+    request.session['access_token'] = access_token
+    return RedirectResponse('/app/')
